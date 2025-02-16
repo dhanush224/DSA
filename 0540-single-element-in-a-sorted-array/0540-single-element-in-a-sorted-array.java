b314abe29d0e39d16b9c373d059b626da2fc7b9f
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-02-15 17:14:45 -0700

    Time: 0 ms (100%), Space: 50 MB (90.14%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,16 +4,32 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 
         int target = -1;
-        int low = 0, high = nums.length-1;
+        int low = 1, high = nums.length-1;
         if(nums.length==1) return nums[0];
-        int size = nums.length-1;
-        for(int i=1;i<nums.length;i=i+2){
-            if(nums[i-1]!=nums[i]) return nums[i-1];
-            
 
+        if(nums.length==1) return nums[0];
+        else{
+            if(nums[0]!=nums[1]) return nums[0];
         }
 
-        return nums[size];
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(mid%2==0){
+                if(nums[mid]==nums[mid-1]) high=mid-1;
+                else if(mid+1!=nums.length){
+                    if(nums[mid]!=nums[mid+1]) return nums[mid];
+                    else low=mid+1;
+                }
+                else return nums[mid];
+            }else{
+                if(nums[mid]==nums[mid-1]) low=mid+1;
+                else high = mid-1;
+            }
+        }
+
+
+        return -1;
 
         
     }
