8bbceaba56ad3b64ac5fdaa83bb5141ca940a716
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-02-22 20:23:26 -0700

    Time: 9 ms (2.05%), Space: 42.5 MB (11.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,62 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        int low = 1, high = nums.length-1;
-        int smallestIndex=0;
-        int rotatedFlag=-1;
-
-        if(nums.length==1){
-            if(nums[0]==target) return 0;
-            else return -1;
-        }
+        int low = 0, high = nums.length-1;
 
         while(low<=high){
-            int mid=low+(high-low)/2;
+            int mid = low+(high-low)/2;
+            System.out.println("Mid: "+mid);
 
-            if(nums[mid-1]>nums[mid]){
-                smallestIndex=mid;
-                rotatedFlag=1;
-                break;
-            }else if(nums[mid]<nums[0]){
-                high=mid-1;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                if(nums[low]<=nums[mid]){
+                    if(nums[low]<=target){
+                        high=mid-1;
+                    }else low = mid+1;
+                }else high=mid-1;
             }else{
-                low=mid+1;
+                if(nums[mid]<=nums[high]){
+                    if(target<=nums[high]){
+                        low=mid+1;
+                    }else high = mid-1;
+                }else low=mid+1;
             }
         }
 
-        System.out.println("Smallest Index: "+smallestIndex);
-
-        if(target<nums[0] || rotatedFlag==-1){
-            low=smallestIndex; high=nums.length-1;
-
-            while(low<=high){
-                int mid=low+(high-low)/2;
-
-                if(nums[mid]==target){
-                    return mid;
-                }else if(nums[mid]>target){
-                    high=mid-1;
-                }else{
-                    low=mid+1;
-                }
-            }
-        }else{
-            low=0; high=smallestIndex-1;
-
-            while(low<=high){
-                int mid=low+(high-low)/2;
-
-                if(nums[mid]==target){
-                    return mid;
-                }else if(nums[mid]>target){
-                    high=mid-1;
-                }else{
-                    low=mid+1;
-                }
-            }
-        }
 
         return -1;
+
     }
  
 }
