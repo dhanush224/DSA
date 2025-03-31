8f39eaf1b103566f8c45ba17c2778971ea21afb6
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-03-30 21:12:15 -0700

    Time: 1 ms (22.91%), Space: 41.5 MB (99.34%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,28 +1,51 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-        int red=0, white=0, blue=0;
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0) red++;
-            if(nums[i]==1) white++;
-            if(nums[i]==2) blue++;
+            if(nums[i]==0){
+                if(map.containsKey(0)) {
+                    int val= map.get(0);
+                    map.put(0, val+1);
+                }
+                else map.put(0,1);
+            }else if(nums[i]==1){
+                if(map.containsKey(1)) {
+                    int val= map.get(1);
+                    map.put(1, val+1);
+                }
+                else map.put(1,1);
+
+            }else{
+                if(map.containsKey(2)) {
+                    int val= map.get(2);
+                    map.put(2, val+1);
+                }
+                else map.put(2,1);
+            }          
         }
 
-        System.out.println("Red: "+red+ " White: "+white+" Blue: "+blue);
+        // System.out.println("Map 0: "+ map.get(0)+" Map 1: "+ map.get(1)+ " Map 2: "+map.get(2));
 
-        for(int i=0;i<nums.length;i++){
-            if(red!=0){
-                nums[i]=0;
-                red--;
-            }else {
-                if(white!=0){
-                    nums[i]=1;
-                    white--;
-                }else nums[i]=2;
-            }
+        int j=0;
+        if(map.containsKey(0)){
+            for(int i=0;i<map.get(0);i++){
+            nums[j]=0;
+            j++;
         }
-
-        
-        
+        }
+        if(map.containsKey(1)){
+            for(int i=0;i<map.get(1);i++){
+            nums[j]=1;
+            j++;
+        }
+        }
+        if(map.containsKey(2)){
+            for(int i=0;i<map.get(2);i++){
+            nums[j]=2;
+            j++;
+        }
+        }
     }
+
 }
