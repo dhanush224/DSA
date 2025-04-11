524c6d20a0f73c7633c8d4d4c2c7f631b30fcc44
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-04-10 17:53:01 -0700

    Time: 2 ms (75%), Space: 53.5 MB (14.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,42 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int first = nums[0];
+        int firstFreq = 1;
+        int sec = nums[0];
+        int secFreq = 1;
+        
 
-        for(int i=0;i<nums.length;i++){
-            Integer ele = nums[i];
-            Integer val = map.get(ele);
-            if(map.containsKey(ele)) map.put(ele, ++val);
-            else map.put(nums[i],1);
+
+        for(int i=1;i<nums.length;i++){
+            // System.out.println("first: "+first+", second: "+sec);
+            if(nums[i]==first){
+                firstFreq++;
+            }else if(nums[i]==sec){
+                secFreq++;
+            }else{
+                sec = nums[i];
+            }
+            // System.out.println("first freq: "+firstFreq+", second freq: "+secFreq);
+
+
+            if(secFreq>firstFreq){
+                int temp = secFreq;
+                secFreq = firstFreq;
+                firstFreq = temp;
+                temp = sec;
+                sec = first;
+                first = temp;
+            }
+            // System.out.println("After swap: "+"first freq: "+first+", second freq: "+sec);
+
+            
+
         }
 
-        for(Integer i: map.keySet()){
-            if(map.get(i)>(nums.length/2)) return i;
-        }
+        return first;
 
-        return -1;
+
         
     }
 }
