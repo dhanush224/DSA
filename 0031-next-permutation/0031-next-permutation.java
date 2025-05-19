6cd1eb8c089ceb7218dfd07c5aa6cf79ede338ca
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-05-19 16:48:29 -0700

    Time: 0 ms (100%), Space: 43.1 MB (74.08%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,39 +1,43 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int index=-1;
+
+        int index = -1;
+
         for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]<nums[i+1]) {
+            if(nums[i]<nums[i+1]){
                 index=i;
                 break;
             }
         }
-        // System.out.println("Index:"+index);
-        if(index==-1)Arrays.sort(nums);
-        else{
-             
+
+        if(index!=-1){
             for(int i=nums.length-1;i>index;i--){
                 if(nums[i]>nums[index]){
-                    int temp = nums[index];
-                    nums[index] = nums[i];
-                    nums[i] = temp;
+                    int temp = nums[i];
+                    nums[i] = nums[index];
+                    nums[index] = temp;
                     break;
                 }
             }
 
-            // System.out.println("After swap:");
-            // for(int i=0;i<nums.length;i++){
-            //     System.out.print(nums[i]+" ");
-            // }
-
             int j=0;
-            for(int i=index+1;i<(nums.length-index-1)/2+index+1;i++){
+            for(int i=index+1;i<index+1+(nums.length-index-1)/2;i++){
                 int temp = nums[i];
                 nums[i] = nums[nums.length-1-j];
                 nums[nums.length-1-j] = temp;
-                j++;                
+                j++;
+                
             }
+        }else{
+
+            for(int i=0;i<nums.length/2;i++){
+                int temp = nums[i];
+                nums[i] = nums[nums.length-1-i];
+                nums[nums.length-1-i] = temp;
+                               
+            }
+
         }
 
-                
     }
 }
