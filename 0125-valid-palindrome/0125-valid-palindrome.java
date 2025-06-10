0c0f46262c5338722fb0ee18a0750376be3ed735
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-06-10 12:46:48 -0700

    Time: 3 ms (72.32%), Space: 43 MB (75.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,8 +3,8 @@ class Solution {
         s=s.toLowerCase();
         int l=0, r=s.length()-1;
         while(l<=r){
-            if(isAlphaNumeric(s.charAt(l))){
-                if(isAlphaNumeric(s.charAt(r)) ){
+            if(isAlphaNumeric1(s.charAt(l))){
+                if(isAlphaNumeric1(s.charAt(r)) ){
                     if((int)s.charAt(l)==(int)s.charAt(r) ){
                         l++;
                         r--;
@@ -24,7 +24,7 @@ class Solution {
         
     }
 
-    public boolean isAlphaNumeric(Character c){
+    public boolean isAlphaNumeric1(Character c){
         if(((int)c>=48 && (int)c<=57) || ( (int)c>=65 && (int)c<=90 ) || ((int)c>=97 && (int)c<=122) ){
             return true;
         }else{
