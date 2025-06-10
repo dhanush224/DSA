fbe2974015c1fcda0e52b2e0d340ffb012c16555
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-06-10 12:45:42 -0700

    Time: 3 ms (72.32%), Space: 43.1 MB (75.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
         int l=0, r=s.length()-1;
         while(l<=r){
-            if(((int)s.charAt(l)>=48 && (int)s.charAt(l)<=57) || ( (int)s.charAt(l)>=65 && (int)s.charAt(l)<=90 ) || ((int)s.charAt(l)>=97 && (int)s.charAt(l)<=122) ){
-                if(((int)s.charAt(r)>=48 && (int)s.charAt(r)<=57) || ( (int)s.charAt(r)>=65 && (int)s.charAt(r)<=90 ) || ((int)s.charAt(r)>=97 && (int)s.charAt(r)<=122) ){
-                    if((int)s.charAt(l)==(int)s.charAt(r) || ((int)s.charAt(l)-32==(int)s.charAt(r) && ((int)s.charAt(r)>=65 && (int)s.charAt(r)<=90 ))|| ( (int)s.charAt(l)==(int)s.charAt(r)-32 && (int)s.charAt(l)>=65 && (int)s.charAt(l)<=90 ) ){
+            if(isAlphaNumeric(s.charAt(l))){
+                if(isAlphaNumeric(s.charAt(r)) ){
+                    if((int)s.charAt(l)==(int)s.charAt(r) ){
                         l++;
                         r--;
                     }else{
@@ -21,5 +22,14 @@ class Solution {
         }
         return true;
         
+    }
+
+    public boolean isAlphaNumeric(Character c){
+        if(((int)c>=48 && (int)c<=57) || ( (int)c>=65 && (int)c<=90 ) || ((int)c>=97 && (int)c<=122) ){
+            return true;
+        }else{
+            return false;
+        }
+
     }
 }
