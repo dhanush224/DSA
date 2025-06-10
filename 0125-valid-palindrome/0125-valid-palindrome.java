81552ad1739c8a072ff1017e68f8310c65f22ba5
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-06-10 12:35:43 -0700

    Time: 2 ms (98.89%), Space: 43.2 MB (68.9%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]","");
-        String new1 = s.toLowerCase();
-        System.out.println("The string: "+new1);
-        for(int i=0;i<(new1.length())/2;i++){
-            if(new1.charAt(i)==(new1.charAt(new1.length()-1-i))){
-                continue;
+        int l=0, r=s.length()-1;
+        while(l<=r){
+            if(((int)s.charAt(l)>=48 && (int)s.charAt(l)<=57) || ( (int)s.charAt(l)>=65 && (int)s.charAt(l)<=90 ) || ((int)s.charAt(l)>=97 && (int)s.charAt(l)<=122) ){
+                if(((int)s.charAt(r)>=48 && (int)s.charAt(r)<=57) || ( (int)s.charAt(r)>=65 && (int)s.charAt(r)<=90 ) || ((int)s.charAt(r)>=97 && (int)s.charAt(r)<=122) ){
+                    if((int)s.charAt(l)==(int)s.charAt(r) || ((int)s.charAt(l)-32==(int)s.charAt(r) && ((int)s.charAt(r)>=65 && (int)s.charAt(r)<=90 ))|| ( (int)s.charAt(l)==(int)s.charAt(r)-32 && (int)s.charAt(l)>=65 && (int)s.charAt(l)<=90 ) ){
+                        l++;
+                        r--;
+                    }else{
+                        return false;
+                    }
+
+                }else{
+                    r--;
+                }
+
+            }else{
+                l++;
             }
-            else
-                return false;
         }
         return true;
+        
     }
 }
