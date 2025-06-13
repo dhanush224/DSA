f79bdeffdf70da5dfaac2fba085efb5e58d522c1
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-06-13 01:58:37 -0700

    Time: 2 ms (93.2%), Space: 47.1 MB (78.52%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,19 +3,16 @@ class Solution {
 
         int[] ans = {-1,-1};
 
-        for(int i=0;i<numbers.length-1;i++){
-            for(int j=i+1;j<numbers.length;j++){
-                // System.out.println("i:"+numbers[i]+", j:"+numbers[j]);
-                if((target-numbers[i])==numbers[j]){
-                    ans[0] = i+1;
-                    ans[1] = j+1;
-                    return ans;
-                }else if(numbers[j]>target-numbers[i]){
-                    break;
-                }
+        int i=0, j=numbers.length-1;
 
-                
+        while(i<j){
+            if(numbers[i]+numbers[j]==target){
+                ans[0] = i+1;
+                ans[1] = j+1;
+                return ans;
             }
+            else if(numbers[i]+numbers[j]<target)i++;
+            else j--;
         }
 
         return ans;
