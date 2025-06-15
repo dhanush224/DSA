f33d9735bdd4a9165b9e342e17ce7d0d76eb7971
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-06-14 18:53:41 -0700

    Time: 42 ms (27.64%), Space: 52.8 MB (14.91%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,54 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        HashSet<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);      
+        
+        List<List<Integer>> ans = new ArrayList<>();
 
-        Map<Integer,Integer> map = new HashMap<>();
+        int prevI=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length-2;i++){
+            if(nums[i]!=prevI){
+                // System.out.println("PrevI: "+prevI);
+                int j=i+1,k=nums.length-1;
+                int prevJ=Integer.MIN_VALUE, prevK=Integer.MIN_VALUE;
+                while(j<k){
+                    int sum = nums[i]+nums[j]+nums[k];
+                    if(sum==0){
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        prevI = nums[i];
+                        prevJ = nums[j];
+                        prevK = nums[k];
+                        // System.out.println("nums[i]:"+nums[i]+", i:"+i);
+                        // System.out.println("nums[j]:"+nums[j]+", j:"+j);
+                        // System.out.println("nums[k]:"+nums[k]+", k:"+k);
+                        // System.out.println("PrevJ: "+prevJ);
+                        // System.out.println("PrevK: "+prevK);        
 
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
-        }
-
-
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                int sum = nums[i]+nums[j];
-                if(map.containsKey(-sum) && map.get(-sum)!=i && map.get(-sum)!=j){
-                    ArrayList<Integer> innerList = new ArrayList<>();
-                    innerList.add(nums[i]);
-                    innerList.add(nums[j]);
-                    innerList.add(nums[map.get(-sum)]);
-                    java.util.Collections.sort(innerList);
-                    set.add(innerList);
+                        j++;
+                        k--;
+                        ans.add(list);
+                    }else if(sum<0){
+                        j++;
+                    }
+                    else{
+                        k--;
+                    }
+                    while(nums[j]==prevJ){
+                        j++;
+                        if(j==nums.length) break;
+                    } 
+                    while(nums[k]==prevK){
+                        k--;
+                        if(k==0) break;
+                    }
                 }
-
             }
         }
 
-        List<List<Integer>> ans = new ArrayList<>(set);
+
 
 
         return ans;
