b1ce6b05622ca291d9c618efc6a74f77b21571dd
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-06-14 21:38:30 -0700

    Time: 0 ms (100%), Space: 42.2 MB (18.87%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,24 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-        int zeroes =0, ones=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0) zeroes++;
-            else if(nums[i]==1) ones++;
-        }
+        int l = 0, r=nums.length-1;
 
         for(int i=0;i<nums.length;i++){
-            if(zeroes!=0){
-                nums[i]=0;
-                zeroes--;
-            } 
-            else if(ones!=0){
-                nums[i]=1;
-                ones--;
+            if(i>r) break;
+            if(nums[i]==0){
+                int temp = nums[i];
+                nums[i] = nums[l];
+                nums[l] = temp;
+                l++;
+            }else if(nums[i]==2){
+                int temp = nums[i];
+                nums[i] = nums[r];
+                nums[r] = temp;
+                r--;
+                i--;
             }
-            else nums[i]=2;
+            // System.out.println("i: "+(i+1)+", l:"+l+", r:"+r);
+            
         }
-
 
     }
 }
