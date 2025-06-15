65c7ff4d0fc558701f3e498bc8ff0c2d37493f7b
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-06-14 19:04:12 -0700

    Time: 30 ms (69.5%), Space: 52.7 MB (19.57%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,12 +5,10 @@ class Solution {
         
         List<List<Integer>> ans = new ArrayList<>();
 
-        int prevI=Integer.MIN_VALUE;
         for(int i=0;i<nums.length-2;i++){
-            if(nums[i]!=prevI){
+            if(i>0 && nums[i]==nums[i-1]) continue;
                 // System.out.println("PrevI: "+prevI);
                 int j=i+1,k=nums.length-1;
-                int prevJ=Integer.MIN_VALUE, prevK=Integer.MIN_VALUE;
                 while(j<k){
                     int sum = nums[i]+nums[j]+nums[k];
                     if(sum==0){
@@ -18,9 +16,6 @@ class Solution {
                         list.add(nums[i]);
                         list.add(nums[j]);
                         list.add(nums[k]);
-                        prevI = nums[i];
-                        prevJ = nums[j];
-                        prevK = nums[k];
                         // System.out.println("nums[i]:"+nums[i]+", i:"+i);
                         // System.out.println("nums[j]:"+nums[j]+", j:"+j);
                         // System.out.println("nums[k]:"+nums[k]+", k:"+k);
@@ -30,22 +25,18 @@ class Solution {
                         j++;
                         k--;
                         ans.add(list);
+
+                        while(j<k && nums[j]==nums[j-1]) j++;
+                        while(j<k && nums[k]==nums[k+1]) k--;
                     }else if(sum<0){
                         j++;
                     }
                     else{
                         k--;
                     }
-                    while(nums[j]==prevJ){
-                        j++;
-                        if(j==nums.length) break;
-                    } 
-                    while(nums[k]==prevK){
-                        k--;
-                        if(k==0) break;
-                    }
+
                 }
-            }
+            
         }
 
 
