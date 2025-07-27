a5ada599dd3fcb79de67c11c5fa164a12c0fbdf7
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-07-26 20:01:54 -0700

    Time: 32 ms (5.39%), Space: 44.3 MB (5.05%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,51 +1,41 @@
 class Solution {
     public int myAtoi(String s) {
 
-        int negFlag=-1;
-        int numFlag = 0;
-        StringBuilder res = new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if((int)s.charAt(i)==45 && negFlag==-1) {
-                negFlag=0;
-                continue;
-            }else if((int)s.charAt(i)==43 && negFlag==-1){
-                negFlag=1;
-                continue;
-            }
-            if(48<=(int)s.charAt(i) && (int)s.charAt(i)<=57){
-                numFlag=1;
-                res.append(s.charAt(i));
-            }else if((int)s.charAt(i)==32 && negFlag==-1)continue;
-            else break;
-            for(int j=i+1;j<s.length();j++){
-                // System.out.println(res+", Ascii: "+(int)s.charAt(j));
-                if((int)s.charAt(j)<48 || (int)s.charAt(j)>57){
-                    break;
-                }else if(48<=(int)s.charAt(j) && (int)s.charAt(j)<=57){
-                    res.append(s.charAt(j));
-                    
+        int i=0;
+        int sign=1;
+        long ans=0;
 
-                } 
+        while(i<s.length() && s.charAt(i)==' ')i++;
 
-            }
-            break;
+
+
+        if(i==s.length()) return 0;
+
+        if(s.charAt(i)=='+') {
+            i++;
         }
+        else if(s.charAt(i)=='-'){
+            sign=-1;
+            i++;
+        }
+        System.out.println("i: "+i+ ", sign:"+sign);
 
-        String res1 = res.toString();
-        System.out.println(res1);
-        if(res1.equals("")) return 0;
+        while(i<s.length() && Character.isDigit(s.charAt(i))){
 
-        int ans = 0;
-        try {
-            ans = Integer.parseInt(res1);
-        } catch (NumberFormatException e) {
-            if(negFlag==0) ans = Integer.MAX_VALUE+1;
-            else ans = Integer.MAX_VALUE;
-        }        
-        
+            int digit = s.charAt(i) - '0';
+            ans = ans*10 + digit;
+            System.out.println(ans+", "+digit);
 
-        if(negFlag==0) return -1*ans;
-        else return ans;
+
+            if(sign*ans>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            else if(sign*ans<Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+            i++;
+        }
+        System.out.println("sign:"+sign);
+
+
+        return (int)(sign*ans);
         
     }
 }
