328626d2d125d183db9312ab429d9c58e4d0a530
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-10-11 15:39:31 -0700

    Time: 43 ms (21.68%), Space: 44.2 MB (82.83%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -2,16 +2,103 @@ class Solution {
     public long subArrayRanges(int[] nums) {
 
         long sum=0;
-        for(int i=0;i<nums.length;i++){
-            int min = nums[i], max = nums[i];
-            for(int j=i;j<nums.length;j++){
-                if(nums[j]>max) max=nums[j];
-                else if(nums[j]<min) min=nums[j];
+        sum= sumMaxSubArray(nums) - sumMinSubArray(nums);
 
-                sum+= (max-min);
-            }
-        }
-        return sum;
-        
+        return sum;        
     }
+
+    public long sumMinSubArray(int[] nums) {
+        long sum=0;
+
+        int[] pse = pse(nums);
+        int[] nsee = nsee(nums);
+
+        for(int i=0;i<nums.length;i++){
+            long left = i-pse[i];
+            long right = nsee[i]-i;
+
+            sum=(sum+ ((left*right)*nums[i]));
+        }
+        return sum;        
+    }
+
+    public long sumMaxSubArray(int[] nums) {
+        long sum=0;
+        int[] pge = pge(nums);
+        int[] ngee = ngee(nums);
+
+        for(int i=0;i<nums.length;i++){
+            long left = i-pge[i];
+            long right = ngee[i]-i;
+
+            sum=(sum +((left*right)*nums[i]));
+        }
+        return sum;        
+    }
+
+    public int[] pse(int[] nums){
+        
+        int n = nums.length;
+        int[] ret = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.peek()]>=nums[i]) st.pop();
+
+            ret[i] = (st.empty()) ? -1:st.peek();
+            st.push(i);
+        }
+
+        return ret;
+    }
+
+    public int[] nsee(int[] nums){
+
+        int n = nums.length;
+        int[] ret = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.peek()]>nums[i]) st.pop();
+
+            ret[i] = (st.empty()) ? n:st.peek();
+            st.push(i);
+        }
+
+        return ret;
+    }
+
+    public int[] pge(int[] nums){
+        
+        int n = nums.length;
+        int[] ret = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.peek()]<=nums[i]) st.pop();
+
+            ret[i] = (st.empty()) ? -1:st.peek();
+            st.push(i);
+        }
+
+        return ret;
+    }
+
+    public int[] ngee(int[] nums){
+
+        int n = nums.length;
+        int[] ret = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.peek()]<nums[i]) st.pop();
+
+            ret[i] = (st.empty()) ? n:st.peek();
+            st.push(i);
+        }
+
+        return ret;
+    }
+
+    
 }
