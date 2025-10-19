2502edf3f40be9010dce7d49362d2052184d897c
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-10-18 17:57:14 -0700

    Time: 40 ms (81.7%), Space: 49.6 MB (88.77%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -1,61 +1,29 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
 
-        int[] pse = pse(arr);
-        int[] nsee = nsee(arr);
+        Stack<Integer> st = new Stack<>();
+        int MOD = (int)Math.pow(10,9) +7;
+        long res=0;
+
         int n = arr.length;
-        int mod = (int)Math.pow(10,9) +7;
-
-        long sum = 0;
         for(int i=0;i<n;i++){
-            long left = i-pse[i];
-            long right = nsee[i]-i;
-            sum= (sum + (left*right*arr[i]) %mod) %mod;
-        }
-
-        return (int)sum;
-        
-    }
-
-    public int[] pse(int[] array){
-        
-        Stack<Integer> st = new Stack<>();
-
-        int n = array.length;
-        int[] ret = new int[n];
-        for(int i=0;i<n;i++){
-
-            while(!st.empty() && array[st.peek()]>=array[i]) st.pop();
-
-            if(st.empty()) ret[i]=-1;
-            else ret[i]=st.peek();
-
+            while(!st.empty() && arr[st.peek()]>arr[i]){
+                int j=st.pop();
+                long left = (!st.empty()) ? j-st.peek() : j+1;
+                long right = i-j;
+                res=(res+(left*right*arr[j]%MOD))%MOD;
+            }
             st.push(i);
-
         }
 
-        return ret;
-
-    }
-
-    public int[] nsee(int[] array){
-
-        Stack<Integer> st = new Stack<>();
-        
-        int n = array.length;
-        int[] ret = new int[n];
-        for(int i=n-1;i>=0;i--){
-
-            while(!st.empty() && array[st.peek()]>array[i]) st.pop();
-
-            if(st.empty()) ret[i]=n;
-            else ret[i]=st.peek();
-
-            st.push(i);
-
+        while(!st.empty()){
+            int j=st.pop();
+            int prev = (!st.empty()) ? st.peek(): -1;
+            long left = j-prev;
+            long right = n-j;
+            res=(res+(left*right*arr[j]%MOD))%MOD;
         }
 
-        return ret;
-
+        return (int)res;
     }
 }
