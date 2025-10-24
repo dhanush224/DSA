b2ba24e3c0bb11060653d8f3e5942993ead48f34
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-10-24 15:41:46 -0700

    Time: 67 ms (1.73%), Space: 115.8 MB (5.11%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,32 @@
+class Solution {
+    double ans=1;
+    public double myPow(double x, int n) {
+
+        if(x==0) return x;
+        if(x==1) return x;
+        if(n==0) return 1;
+
+        if(x==-1){
+            if(n%2==0) return -1*x;
+            else return x;
+        } 
+
+        if(n==Integer.MAX_VALUE && x<1) return 0.0;
+        else if(n<=-200000000) return 0.0;
+
+        if(n==0) return ans;
+
+        if(n>0){
+            ans*=x;
+            n--;
+            myPow(x,n);
+        }else{
+            ans/=x;
+            n++;
+            myPow(x,n);
+        }
+
+        return ans;
+      
+    }
+}
