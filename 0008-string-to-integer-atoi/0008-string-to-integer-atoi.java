21490644a180d46e7ea31ae737d7171d86f5f1b2
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2025-10-24 15:13:21 -0700

    Time: 1 ms (100%), Space: 42.8 MB (13.36%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,51 +1,47 @@
 class Solution {
+    int i=0,whitespace=0,sign=1,num=0;
+    long ans=0;
     public int myAtoi(String s) {
 
-        int negFlag=-1;
-        int numFlag = 0;
-        StringBuilder res = new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if((int)s.charAt(i)==45 && negFlag==-1) {
-                negFlag=0;
-                continue;
-            }else if((int)s.charAt(i)==43 && negFlag==-1){
-                negFlag=1;
-                continue;
+        if(i<s.length() && whitespace==0 && s.charAt(i)==' '){
+            i++;
+            myAtoi(s);
+        }else if(i<s.length() && whitespace==0 && s.charAt(i)!=' '){
+            whitespace=1;
+            myAtoi(s);
+        }else if(i<s.length() && whitespace==1 && s.charAt(i)=='-' && num==0){
+            sign=-1;
+            i++;
+            num=1;
+            myAtoi(s);
+        }else if(i<s.length() && whitespace==1 && s.charAt(i)=='+' && num==0){
+            sign=1;
+            i++;
+            num=1;
+            myAtoi(s);
+        }else if(i<s.length() && whitespace==1 && Character.isDigit(s.charAt(i))){
+            num=1;
+            int digit = s.charAt(i) - '0';
+            ans=ans*10+digit;
+            if(sign*ans>=Integer.MAX_VALUE) {
+                ans=Integer.MAX_VALUE;
+                return (int)ans;
             }
-            if(48<=(int)s.charAt(i) && (int)s.charAt(i)<=57){
-                numFlag=1;
-                res.append(s.charAt(i));
-            }else if((int)s.charAt(i)==32 && negFlag==-1)continue;
-            else break;
-            for(int j=i+1;j<s.length();j++){
-                // System.out.println(res+", Ascii: "+(int)s.charAt(j));
-                if((int)s.charAt(j)<48 || (int)s.charAt(j)>57){
-                    break;
-                }else if(48<=(int)s.charAt(j) && (int)s.charAt(j)<=57){
-                    res.append(s.charAt(j));
-                    
-
-                } 
-
+            else if(sign*ans<=Integer.MIN_VALUE){
+                ans= Integer.MIN_VALUE;
+                return (int)ans;
             }
-            break;
+            i++;
+            myAtoi(s);
+        }else if(i<s.length() && num==1 && !Character.isDigit(s.charAt(i))){
+
+            return sign*(int)ans;            
         }
 
-        String res1 = res.toString();
-        System.out.println(res1);
-        if(res1.equals("")) return 0;
 
-        int ans = 0;
-        try {
-            ans = Integer.parseInt(res1);
-        } catch (NumberFormatException e) {
-            if(negFlag==0) ans = Integer.MAX_VALUE+1;
-            else ans = Integer.MAX_VALUE;
-        }        
-        
+        if(ans>=Integer.MAX_VALUE) return sign*Integer.MAX_VALUE;
 
-        if(negFlag==0) return -1*ans;
-        else return ans;
-        
+
+        return sign*(int)ans;      
     }
 }
