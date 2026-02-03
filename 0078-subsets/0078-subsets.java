da1ad27a393d3df585df4048ce65d3b9c53d0590
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-02-02 22:48:00 -0700

    Time: 1 ms (87.95%), Space: 44.5 MB (5.91%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -3,21 +3,21 @@ class Solution {
 
         List<List<Integer>> outerList =new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        printSubsets(0,list,nums,outerList,0);   
+        printSubsets(0,list,nums,outerList);   
 
         return outerList;     
     }
 
-    public void printSubsets(int i, List<Integer> list, int[] nums, List<List<Integer>> outerList, int sum) {
+    public void printSubsets(int i, List<Integer> list, int[] nums, List<List<Integer>> outerList) {
 
         if(i==nums.length){
             outerList.add(new ArrayList<>(list));
             return;
         }
         list.add(nums[i]);
-        printSubsets(i+1,list,nums,outerList,0);
+        printSubsets(i+1,list,nums,outerList);
         list.remove(list.size()-1);
-        printSubsets(i+1,list,nums,outerList,0);
+        printSubsets(i+1,list,nums,outerList);
 
 
     }
