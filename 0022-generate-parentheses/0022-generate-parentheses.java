a3144588cee4eb6e9ad8f9f84a3432fb7544c691
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-02-02 23:26:11 -0700

    Time: 14 ms (5.22%), Space: 44.8 MB (51.3%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        recursion(n, list, sb, 0,0);
+
+        return list;
+    }
+
+    public void recursion(int n, List<String> list, StringBuilder sb, int openN, int closedN) {
+
+        if(openN==closedN && closedN==n){
+            list.add(sb.toString());
+            return;
+        }
+
+
+        if(openN==n && closedN<n){
+            sb.append(')');
+            recursion(n,list,sb,openN,closedN+1);
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        if(openN>=closedN && openN<n){
+            System.out.println("open:"+openN);
+            sb.append('(');
+            recursion(n,list,sb,openN+1,closedN);
+            sb.deleteCharAt(sb.length()-1);
+            if(openN>closedN){
+                sb.append(')');
+                recursion(n,list,sb,openN,closedN+1);
+                sb.deleteCharAt(sb.length()-1);
+            }
+
+        }
+
+    }
+}
