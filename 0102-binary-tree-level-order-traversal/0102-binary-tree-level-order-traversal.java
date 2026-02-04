49a3b3651c3d14f1a9a82a2b5fc470cb770e226d
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-02-03 22:06:36 -0700

    Time: 2 ms (22.67%), Space: 46.9 MB (35.1%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,29 +14,24 @@
  * }
  */
 class Solution {
-    Queue<TreeNode> q = new LinkedList<>();
-    List<List<Integer>> outerList = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
-
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> outerList = new ArrayList<>();
         if(root==null) return outerList;
 
-        List<Integer> list = new ArrayList<>();
-
         if(q.size()==0) q.add(root);
-
-        int n = q.size();
-
-        for(int i=0;i<n;i++){
-            if(q.peek().left!=null) q.add(q.peek().left);
-            if(q.peek().right!=null) q.add(q.peek().right);
-            list.add(q.remove().val);
-            
+        
+        while(q.size()!=0){
+            int n = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                if(q.peek().left!=null) q.add(q.peek().left);
+                if(q.peek().right!=null) q.add(q.peek().right);
+                list.add(q.remove().val);
+            }
+            outerList.add(new ArrayList<>(list));
         }
 
-        outerList.add(new ArrayList<>(list));
-        if(q.size()==0) return outerList;
-        levelOrder(root);
-        
         return outerList;
 
     }
