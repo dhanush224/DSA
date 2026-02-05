a2bd6821c6d15f0a0a6b22c1df45a6b90df0bb75
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-02-04 22:56:23 -0700

    Time: 2 ms (92.59%), Space: 57.2 MB (42.04%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+
+        if(root.left==null && root.right==null) return 1;
+        int total=recursion(root,Integer.MIN_VALUE,0);
+
+        // if(root.left!=null & root.right!=null) total++;
+        return total;
+        
+    }
+
+    public int recursion(TreeNode node, int highestUntilNow, int total){
+
+        if(node==null) return 0;
+        total =0;
+        if(node.val>=highestUntilNow) {
+            highestUntilNow=node.val;
+            total=1;
+        }    
+
+        total += recursion(node.left,highestUntilNow,total);
+        total += recursion(node.right,highestUntilNow,total);
+
+        // System.out.println("Node Value: "+node.val+"; X: "+x+", Y: "+y);
+
+        return total;
+
+    }
+}
