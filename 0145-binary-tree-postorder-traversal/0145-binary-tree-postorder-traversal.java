ec06f6dd802a38def2617c53ae36815871eb1321
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-02-10 02:02:46 -0700

    Time: 1 ms (14.26%), Space: 43 MB (84.66%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,16 +14,55 @@
  * }
  */
 class Solution {
-    List<Integer> list = new ArrayList<>();
     public List<Integer> postorderTraversal(TreeNode root) {
 
+        List<Integer> list = new ArrayList<>();
         if(root==null) return list;
 
-        postorderTraversal(root.left);
-        postorderTraversal(root.right);
-        list.add(root.val);
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode node = root;
+        st.push(root);
+        node=node.left;
 
-        return list;
-     
+        while(!st.empty()){
+
+            if(node!=null){
+                if(!st.empty() && st.peek()==node){
+                    TreeNode right = node.right;
+                    node=right;              
+                    // if(right!=null && right.val!=list.get(list.size()-1)){
+                    //     node=right;
+                    // }
+                    // else{
+                    //     list.add((st.pop().val));
+                    //     if(st.empty())break;
+                    //     node=st.peek();
+                    // }
+                }else{
+                    st.push(node);
+                    node=node.left;
+                }
+            }
+            else{
+                if(st.empty()) break;
+                node=st.peek();
+                node=node.right;
+                if(node==null){
+                    TreeNode temp = st.pop();
+                    list.add(temp.val);
+                    if(st.empty()) break;
+                    node=st.peek();
+                    while(!st.empty() && node.right==temp){
+                        temp = st.pop();
+                        list.add(temp.val);
+                        if(st.empty()) break;
+                        node=st.peek();
+                    }
+
+                }
+            }
+        }
+
+        return list;     
     }
 }
