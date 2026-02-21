72c8a76693094956bd068c967d038dfc67ee7c16
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-02-21 01:24:05 -0700

    Time: 2 ms (1.8%), Space: 43.7 MB (62.5%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        List<List<Integer>> outerList = new ArrayList<>();
+        if(root==null) return outerList;
+
+        Deque<TreeNode> q = new ArrayDeque<>();
+        
+        q.add(root);
+        int level=0;
+
+        while(q.size()!=0){
+            int n = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(TreeNode node: q){
+                list.add(node.val);
+            }
+            outerList.add(new ArrayList<>(list));
+            if(level%2==0){
+                Deque<TreeNode> newqueue = new ArrayDeque<>();
+                for(int i=0;i<n;i++){
+                    TreeNode node = q.removeLast();
+                    newqueue.add(node);
+                }  
+                for(int i=0;i<n;i++){
+                    TreeNode node = newqueue.remove();
+                    if(node.right!=null) q.add(node.right);
+                    if(node.left!=null) q.add(node.left);
+                }
+            }else{
+                Deque<TreeNode> newqueue = new ArrayDeque<>();
+                for(int i=0;i<n;i++){
+                    TreeNode node = q.removeLast();
+                    newqueue.add(node);
+                }   
+                for(int i=0;i<n;i++){
+                    TreeNode node = newqueue.remove();
+                    if(node.left!=null) q.add(node.left);
+                    if(node.right!=null) q.add(node.right);
+                }
+            }
+            level++;
+        }
+
+        return outerList;        
+    }
+
+}
