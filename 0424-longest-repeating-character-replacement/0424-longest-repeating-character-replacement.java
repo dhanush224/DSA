8e490239bb585b9cfa2163aeea031879db34b43c
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-03-20 14:00:22 -0700

    Time: 29 ms (20.72%), Space: 46.8 MB (27.65%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -4,24 +4,25 @@ class Solution {
 
         Map<Integer,Integer> map = new HashMap<>();
         int maxLength=0;
+        int maxFreq=0;
 
         int i =0;
         int n = s.length();
         for(int j=0;j<n;j++){
+            if(j==n-1 && (j-i+1)<maxLength)break;
+
             int key = (int)s.charAt(j);
             if(!map.containsKey(key)) map.put(key,1);
             else map.put(key,map.get(key)+1);
 
-            int length = j-i+1;
-            while(length-maxCount(map)>k &&i<n){
+            maxFreq=Math.max(maxFreq,map.get(key));
+
+            while((j-i+1)-maxFreq>k){
                 int updateKey = (int)s.charAt(i);
                 map.put(updateKey,map.get(updateKey)-1);
                 i++;
-                length=j-i+1;
-                if(j==n-1 && length<maxLength)break;
             }
-            if(i==n)break;
-            maxLength=Math.max(length,maxLength);
+            maxLength=Math.max(j-i+1,maxLength);
             
         }
 
