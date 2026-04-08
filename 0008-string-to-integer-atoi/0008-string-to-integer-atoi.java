41dbd674b8af25a7cdd01f2272425450035b4f69
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-04-08 13:16:01 -0700

    Time: 1 ms (100%), Space: 44 MB (31.22%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,47 +1,49 @@
 class Solution {
-    int i=0,whitespace=0,sign=1,num=0;
-    long ans=0;
     public int myAtoi(String s) {
 
-        if(i<s.length() && whitespace==0 && s.charAt(i)==' '){
-            i++;
-            myAtoi(s);
-        }else if(i<s.length() && whitespace==0 && s.charAt(i)!=' '){
-            whitespace=1;
-            myAtoi(s);
-        }else if(i<s.length() && whitespace==1 && s.charAt(i)=='-' && num==0){
+        int i=0, sign=1;
+        int n=s.length();
+        if(n==0) return 0;
+
+        while(i<n && s.charAt(i)==' ')i++;
+        if(i<n && s.charAt(i)=='-'){
             sign=-1;
             i++;
-            num=1;
-            myAtoi(s);
-        }else if(i<s.length() && whitespace==1 && s.charAt(i)=='+' && num==0){
-            sign=1;
-            i++;
-            num=1;
-            myAtoi(s);
-        }else if(i<s.length() && whitespace==1 && Character.isDigit(s.charAt(i))){
-            num=1;
-            int digit = s.charAt(i) - '0';
-            ans=ans*10+digit;
-            if(sign*ans>=Integer.MAX_VALUE) {
-                ans=Integer.MAX_VALUE;
-                return (int)ans;
-            }
-            else if(sign*ans<=Integer.MIN_VALUE){
-                ans= Integer.MIN_VALUE;
-                return (int)ans;
-            }
-            i++;
-            myAtoi(s);
-        }else if(i<s.length() && num==1 && !Character.isDigit(s.charAt(i))){
-
-            return sign*(int)ans;            
+        }else if(i<n && s.charAt(i)=='+')i++;
+        while(i<n && s.charAt(i)=='0')i++;
+        if(i==n) return 0;
+        long val=0;
+        int multiplier=1;
+        val = recursion(i,val,multiplier,s);
+        if(val==Integer.MAX_VALUE){
+            return sign*Integer.MAX_VALUE;
+        } else if(val==Integer.MIN_VALUE){
+            if(sign==-1) return Integer.MIN_VALUE;
+            else return Integer.MAX_VALUE;
         }
+        else return sign*(int)val;
+            
+    }
 
+    public long recursion(int i, long val, int m, String s){
+        
+        if(val>Integer.MAX_VALUE || val<=Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
+        }else if(val==Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(i==s.length()){
+            return val;
+        }
+        Character c = s.charAt(i);
+        
 
-        if(ans>=Integer.MAX_VALUE) return sign*Integer.MAX_VALUE;
+        if(Character.isDigit(c)){
+            int x = (int)c - 48;
+            val=val*m+x;
+            if(m==1)m*=10;
+            val=recursion(i+1,val,m,s);
+        }else return val;
 
+        return val;
 
-        return sign*(int)ans;      
     }
 }
