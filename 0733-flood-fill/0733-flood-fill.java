9a8a52687168685285dae72a5c395b064a64971f
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-07-22 18:06:31 -0400

    Time: 1 ms (39.11%), Space: 47.2 MB (11.25%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+        if(image[sr][sc]==color) return image;
+
+        Queue<int[]> q = new ArrayDeque<>();
+        int m = image.length;
+        int n = image[0].length;
+        int[][] ret = new int[m][n];
+        int original=image[sr][sc];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ret[i][j] = image[i][j];
+            }
+        }
+
+        ret[sr][sc] = color;
+        q.add(new int[]{sr,sc});
+
+        int[][] dirs = new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int[] arr = q.peek();
+                int r=arr[0];
+                int c=arr[1];
+
+                q.remove();
+
+
+                for(int[] d: dirs){
+                    int nr = r+d[0];
+                    int nc = c+d[1];
+
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && ret[nr][nc]==original){
+                        ret[nr][nc]=color;
+                        q.add(new int[]{nr,nc});
+                    }
+                }
+            }
+
+        }
+
+        return ret;
+        
+    } 
+}
