bd81623a1afefb81044dde80bad5a17c1c8f9d55
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-07-27 13:04:16 -0400

    Time: 2 ms (90.33%), Space: 44 MB (85.24%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        Queue<int[]> q = new ArrayDeque<>();
+
+        boolean problem = false;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1) problem=true;
+            }
+        } 
+        if(problem==false) return 0;
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==2) q.add(new int[]{i,j});
+            }
+        } 
+
+        int time=0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+
+                int[] front = q.remove();
+                for(int[] d: dirs){
+
+                    int nr = front[0] + d[0];
+                    int nc = front[1] + d[1];
+
+
+
+                    if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]==1){
+                        grid[nr][nc]=2;
+
+                        q.add(new int[]{nr,nc});
+                    }
+                }
+            }
+            time++;
+        }
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1) return -1;
+            }
+        } 
+
+
+        return time-1;
+    }
+}
