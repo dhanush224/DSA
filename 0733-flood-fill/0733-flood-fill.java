a596ed45baa1201648d4ef133cbdd96b78835bcb
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-07-28 20:56:53 -0400

    Time: 1 ms (38.61%), Space: 47.1 MB (11.18%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,42 +1,27 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-
         if(image[sr][sc]==color) return image;
 
         Queue<int[]> q = new ArrayDeque<>();
-        int m = image.length;
-        int n = image[0].length;
-        int[][] ret = new int[m][n];
-        int original=image[sr][sc];
+        q.add(new int[]{sr,sc});
+        int original = image[sr][sc];
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ret[i][j] = image[i][j];
+        int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.isEmpty()){
+            int[] front = q.remove();
+            image[front[0]][front[1]]=color;
+
+
+            for(int[] d: dirs){
+                int nr = front[0]+d[0];
+                int nc = front[1]+d[1];
+                
+                if(nr>=0 && nr<image.length && nc>=0 && nc<image[0].length && image[nr][nc]==original){
+                    q.add(new int[]{nr,nc});
+                }
             }
         }
 
-        ret[sr][sc] = color;
-        q.add(new int[]{sr,sc});
-
-        int[][] dirs = new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
-        while(!q.isEmpty()){
-                int[] arr = q.poll();
-                int r=arr[0];
-                int c=arr[1];
-
-                for(int[] d: dirs){
-                    int nr = r+d[0];
-                    int nc = c+d[1];
-
-                    if(nr>=0 && nr<m && nc>=0 && nc<n && ret[nr][nc]==original){
-                        ret[nr][nc]=color;
-                        q.add(new int[]{nr,nc});
-                    }
-                }
-
-        }
-
-        return ret;
-        
+        return image;
     } 
 }
