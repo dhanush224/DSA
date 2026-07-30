56f59b9ba421d0c95f6f3f621e1bbd781a7c0c96
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-07-30 15:29:36 -0400

    Time: 4 ms (45.69%), Space: 51.9 MB (75.93%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,11 +4,12 @@ class Solution {
         int n = grid[0].length;
         int[][] vis = new int[m][n];
         int islands=0;
+        int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' && vis[i][j]!=1){
-                    bfs(i,j,grid,vis,m,n);
+                    dfs(i,j,grid,vis,m,n,dirs);
                     islands++;
                 }
             }
@@ -16,25 +17,17 @@ class Solution {
         return islands;      
     }
 
-    public void bfs(int i, int j, char[][] grid, int[][] vis,int m, int n){
+    public void dfs(int i, int j, char[][] grid, int[][] vis, int m, int n, int[][] dirs){
         vis[i][j]=1;
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{i,j});
-        int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
 
-        while(!q.isEmpty()){
-            int[] curr = q.remove();
+        for(int[]  d: dirs){
+            int nr = i+d[0];
+            int nc = j+d[1];
 
-            for(int[] d: dirs){
-                int nr = curr[0] + d[0];
-                int nc = curr[1] + d[1];
-
-                if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1' && vis[nr][nc]!=1){
-                    vis[nr][nc]=1;
-                    q.add(new int[]{nr,nc});
-                } 
-            }
+            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1' && vis[nr][nc]!=1) dfs(nr,nc,grid,vis,m,n,dirs);
         }
 
+
     }
+
 }
