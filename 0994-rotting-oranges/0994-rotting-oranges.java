24b6034b18a47ea561a93f716bc1b3cc80f6f028
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-08-04 14:53:02 -0400

    Time: 2 ms (89.92%), Space: 44.1 MB (56.84%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,52 +2,46 @@ class Solution {
     public int orangesRotting(int[][] grid) {
 
         Queue<int[]> q = new ArrayDeque<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        int rotten = 0;
+        int fresh = 0;
+        int[][] vis = new int[m][n];
+        int minutes=0;
 
-        boolean problem = false;
-        int freshOranges=0;
-
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1) {
-                    problem=true;
-                    freshOranges++;
-                }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    rotten++;
+                    q.add(new int[]{i,j});
+                    vis[i][j]=1;
+                }else if(grid[i][j]==1) fresh++;
             }
-        } 
-        if(problem==false) return 0;
+        }
 
+        if(fresh==0) return 0;
 
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==2) q.add(new int[]{i,j});
-            }
-        } 
-
-        int time=0;
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
 
         while(!q.isEmpty()){
             int size = q.size();
-            for(int i=0;i<size;i++){
-
-                int[] front = q.remove();
+            for(int k=0;k<size;k++){
+                int[] curr = q.remove();
                 for(int[] d: dirs){
+                    int nr = curr[0]+d[0];
+                    int nc = curr[1]+d[1];
 
-                    int nr = front[0] + d[0];
-                    int nc = front[1] + d[1];
-
-                    if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]==1){
+                    if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1 && vis[nr][nc]!=1){
                         grid[nr][nc]=2;
+                        fresh--;
                         q.add(new int[]{nr,nc});
-                        freshOranges--;
                     }
                 }
             }
-            time++;
-            if(freshOranges==0) break; 
+            minutes++;
         }
 
-        if(freshOranges!=0) return -1;
-        else return time;
+        if(fresh!=0) return -1;
+        else return minutes-1;
     }
 }
