b2dfb6dbadd8cf1b39dfc56a8e9e51344bdeee5c
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-08-10 04:57:47 -0400

    Time: 0 ms (100%), Space: 47.2 MB (30.33%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -15,64 +15,26 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-
         if(root==null) return new TreeNode(val);
-
         TreeNode node = root;
-        int floor=-1;
-        int ceil=-1;
+        TreeNode n = new TreeNode(val);
 
-        while(node!=null){
-            if(node.val<val){
-                floor=node.val;
-                node=node.right;
+        while(true){
+            if(node.val>val){
+                if(node.left!=null) node=node.left;
+                else{
+                    node.left=n;
+                    break;
+                }
             }else{
-                ceil=node.val;
-                node=node.left;
-            }
-        }
-
-
-        node=root;
-
-        if(floor==-1){
-            while(node.left!=null) node=node.left;
-            node.left=new TreeNode(val);
-        }else if(ceil==-1){
-            while(node.right!=null) node=node.right;
-            node.right=new TreeNode(val);
-        }else{
-            while(node!=null){
-                // if(node.val==floor && node.right!=null && node.right.val==ceil){
-                //     TreeNode temp = node.right;
-                //     node.right=new TreeNode(val);
-                //     node.right.right=temp;
-                //     break;
-                // }else if(node.val==ceil && node.left!=null && node.left.val==floor){
-                //     TreeNode temp = node.left;
-                //     node.left = new TreeNode(val);
-                //     node.left.left=temp;
-                //     break;
-                // }else 
-                if(node.val<val){
-                    if(node.right!=null)
-                        node=node.right;
-                    else {
-                        node.right=new TreeNode(val);
-                        break;
-                    }
-                }else if(node.val>val){
-                    if(node.left!=null)
-                        node=node.left;
-                    else{
-                        node.left=new TreeNode(val);
-                        break;
-                    } 
+                if(node.right!=null) node=node.right;
+                else{
+                    node.right=n;
+                    break;
                 }
             }
-        }
+        }   
 
         return root;
-        
     }
 }
