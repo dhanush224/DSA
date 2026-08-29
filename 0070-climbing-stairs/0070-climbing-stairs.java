ad508a76eae9870f6a6eee429bb280ed97ff3531
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-08-29 17:15:16 -0400

    Time: 0 ms (100%), Space: 42.1 MB (54.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memory = new int[n+1];
-        memory[1]=1;
         int prev = 1;
         int prev2 = 1;
-        for(int i=1;i<=n;i++){
-            if(memory[i]==0){
-                memory[i]=prev+prev2;
+        for(int i=2;i<=n;i++){
+                int curr=prev+prev2;
                 prev2=prev;
-                prev=memory[i];
-            }
+                prev=curr;
+            
         }
 
-        return memory[n];
+        return prev;
         
     }
 }
