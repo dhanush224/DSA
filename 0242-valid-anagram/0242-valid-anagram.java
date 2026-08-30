08df5fd3606553966a2747100ae876d106f1d148
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-08-30 01:24:49 -0400

    Time: 17 ms (16.7%), Space: 46.3 MB (42.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,23 +4,22 @@ class Solution {
         if(s.length()!=t.length()) return false;
 
         Map<Integer, Integer> map1 = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map2 = new HashMap<Integer, Integer>();
 
         for(int i=0;i<s.length();i++){
             Integer key = (int)s.charAt(i);
-            map1.put(key, map1.getOrDefault(key,0)+1);
+            if(map1.containsKey(key)) map1.put(key, map1.get(key)+1);
+            else map1.put(key, 1);
         }
 
         for(int i=0;i<t.length();i++){
             Integer key = (int)t.charAt(i);
-            if(!map1.containsKey(key)) return false;
-            map1.put(key, map1.get(key)-1);            
+            if(map2.containsKey(key)) map2.put(key, map2.get(key)+1);
+            else map2.put(key, 1);
         }
 
-        for(int key: map1.keySet()){
-            if(map1.get(key)!=0) return false;
-        }
+        return map1.equals(map2);
 
-        return true;
 
        
     }
