3c5482dff7d0cadd79a87c544b03146769fdfa0b
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-08-30 01:19:55 -0400

    Time: 15 ms (23.97%), Space: 46.1 MB (49.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,27 +4,20 @@ class Solution {
         if(s.length()!=t.length()) return false;
 
         Map<Integer, Integer> map1 = new HashMap<Integer, Integer>();
-        Map<Integer, Integer> map2 = new HashMap<Integer, Integer>();
 
         for(int i=0;i<s.length();i++){
             Integer key = (int)s.charAt(i);
-            if(map1.containsKey(key)) map1.put(key, map1.get(key)+1);
-            else map1.put(key, 1);
+            map1.put(key, map1.getOrDefault(key,0)+1);
         }
 
         for(int i=0;i<t.length();i++){
             Integer key = (int)t.charAt(i);
-            if(map2.containsKey(key)) map2.put(key, map2.get(key)+1);
-            else map2.put(key, 1);
+            if(!map1.containsKey(key)) return false;
+            map1.put(key, map1.get(key)-1);            
         }
 
-        System.out.println(map1);
-        System.out.println(map2);
-
-        for(int i=0;i<s.length();i++){
-            Integer key = (int)s.charAt(i);
-            // System.out.println(map1.get(key)+", "+map2.get(key));
-            if(!map1.get(key).equals(map2.get(key))) return false;
+        for(int key: map1.keySet()){
+            if(map1.get(key)!=0) return false;
         }
 
         return true;
