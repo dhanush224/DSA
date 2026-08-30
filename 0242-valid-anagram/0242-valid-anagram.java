ed04389e8648aeb1af54d89dded53202649b8cdf
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-08-30 01:27:12 -0400

    Time: 21 ms (7.29%), Space: 46.2 MB (47.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,17 +3,17 @@ class Solution {
 
         if(s.length()!=t.length()) return false;
 
-        Map<Integer, Integer> map1 = new HashMap<Integer, Integer>();
-        Map<Integer, Integer> map2 = new HashMap<Integer, Integer>();
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
 
         for(int i=0;i<s.length();i++){
-            Integer key = (int)s.charAt(i);
+            char key = s.charAt(i);
             if(map1.containsKey(key)) map1.put(key, map1.get(key)+1);
             else map1.put(key, 1);
         }
 
         for(int i=0;i<t.length();i++){
-            Integer key = (int)t.charAt(i);
+            char key = t.charAt(i);
             if(map2.containsKey(key)) map2.put(key, map2.get(key)+1);
             else map2.put(key, 1);
         }
