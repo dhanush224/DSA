ad383ac1037d03f3b0ecfe291081cec43eafb66c
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-06 17:53:32 -0700

    Time: 7 ms (35.6%), Space: 45.1 MB (78.96%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,35 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-
-        Arrays.sort(candidates);
-
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> innerList = new ArrayList<>();
-        recursion(list,innerList, candidates, target, 0, 0);
+        Arrays.sort(candidates);
+        recursion(list, innerList, candidates, 0, target, 0);
 
-        return list;
+        return list;  
+    }
 
-    }    
-
-    public void recursion(List<List<Integer>> list, List<Integer> innerList, int[] candidates, int target, int i, int sum){
+    public void recursion(List<List<Integer>> list, List<Integer> innerList, int[] candidates, int i, int target, int sum){
 
         if(sum==target){
             list.add(new ArrayList<>(innerList));
             return;
         }
-        if(sum>target || i==candidates.length){
-            return;
-        }
-
+        if(sum>target || i==candidates.length) return;
+        
         innerList.add(candidates[i]);
-        recursion(list, innerList, candidates, target, i+1, sum+candidates[i]);
-
+        recursion(list, innerList, candidates, i+1, target, sum+candidates[i]);
+        while(i<candidates.length-1 && candidates[i]==candidates[i+1])i++;
         innerList.remove(innerList.size()-1);
-        while(i<candidates.length-1 && candidates[i]==candidates[i+1]) i++;
-        recursion(list, innerList, candidates, target, i+1, sum);
+        recursion(list,innerList,candidates,i+1,target, sum);
 
         return;
-
-
     }
+
 }
