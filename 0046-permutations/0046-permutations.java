48eba102289799bcc0419f3f0489ef22ca160388
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-07 21:08:20 -0700

    Time: 2 ms (34.9%), Space: 45.4 MB (62.44%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        recursion(nums, list, new LinkedHashSet<>());
+        recursion(nums, list, new ArrayList<>(), new boolean[nums.length]);
 
         return list;        
     }
 
-    public void recursion(int[] nums, List<List<Integer>> list, Set<Integer> set){
+    public void recursion(int[] nums, List<List<Integer>> list, List<Integer> set, boolean[] pick){
         if(set.size()==nums.length){
             list.add(new ArrayList<>(set));
             return;
         }
 
         for(int i=0;i<nums.length;i++){
-            if(!set.add(nums[i])) continue;
-            recursion(nums,list,set);
-            set.remove(nums[i]);
+            if(pick[i]==false){
+                set.add(nums[i]);
+                pick[i]=true;
+                recursion(nums,list,set,pick);
+                set.remove(set.size()-1);
+                pick[i]=false;
+            }
         }
 
         return;
