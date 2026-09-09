10668bf58535fac5b8d8b84724fb0527a51def76
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-08 19:06:26 -0700

    Time: 2 ms (99.73%), Space: 44.9 MB (76.25%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,29 +1,22 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-
         List<List<Integer>> list = new ArrayList<>();
-        List<Integer> innerList = new ArrayList<>();
-
         Arrays.sort(nums);
-        recursion(list, innerList, nums, 0);
-
+        recursion(list, new ArrayList<>(), nums, 0);
         return list;
     }
 
-    public void recursion(List<List<Integer>> list, List<Integer> innerList, int[] nums, int i){
-        
+    public void recursion(List<List<Integer>> list, List<Integer> es, int[] nums, int i){
         if(i==nums.length){
-            list.add(new ArrayList<>(innerList));
+            list.add(new ArrayList<>(es));
             return;
         }
 
-        innerList.add(nums[i]);
-        recursion(list, innerList, nums,i+1);
+        es.add(nums[i]);
+        recursion(list,es,nums,i+1);
+        es.remove(es.size()-1);
+        while(i+1<nums.length && nums[i]==nums[i+1]) i++;
+        recursion(list,es,nums,i+1);
 
-        innerList.remove(innerList.size()-1);
-        while(i<nums.length-1 && nums[i]==nums[i+1])i++;
-        recursion(list, innerList, nums, i+1);
-
-        return;
     }
 }
