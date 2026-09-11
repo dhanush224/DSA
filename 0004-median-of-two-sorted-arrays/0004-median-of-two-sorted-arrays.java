cc27d72c7f0c699c0bd5aa73afda403c8e7006b0
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-10 19:04:15 -0700

    Time: 8 ms (13.35%), Space: 48.7 MB (70.85%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,39 +1,41 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        ArrayList<Integer> arr = new ArrayList<>();
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {  
 
-        for(int i=0,j=0; i<nums1.length || j<nums2.length; i++,j++){
-            // System.out.println("Entered loop");
-            if(i>=nums1.length) {
-                // System.out.println("Inside first if");
-                arr.add(nums2[j]);
-                // System.out.println(arr); 
-                continue;
-            }
-            if(j>=nums2.length){
-                arr.add(nums1[i]);
-                // System.out.println(arr); 
-                continue;
-            } 
 
-            if(nums1[i]<nums2[j]){
-                arr.add(nums1[i]);
-                j--;
+        if(nums1.length>nums2.length){
+            int[] temp = nums1;
+            nums1=nums2;
+            nums2=temp;
+        }
+        int m = nums1.length;
+        int n = nums2.length;
+        int low=0;
+        int high=m-1;
+        int length = (m+n)/2;
+
+        while(true){
+            int mid1 = (int) Math.floor((high+low)/2.0);
+            int mid2 = length-mid1-2;
+            int left1 = (mid1>=0) ? nums1[mid1] : Integer.MIN_VALUE;
+            int right1 = (mid1+1<m)? nums1[mid1+1] : Integer.MAX_VALUE;
+            int left2 = (mid2>=0) ? nums2[mid2] : Integer.MIN_VALUE;
+            int right2 = (mid2+1<n) ? nums2[mid2+1] : Integer.MAX_VALUE;
+
+
+            if(left1<=right2 && left2<=right1){
+                if((m+n)%2==0) return (Math.max(left1,left2)+Math.min(right1,right2))/(double)2;
+                else return Math.min((double)right1,(double)right2);
+            }else if(left1>right2){
+                high=mid1-1;
+                System.out.println("high:"+high+","+low);
             }else{
-                arr.add(nums2[j]);
-                i--;
+                low=mid1+1;
             }
-            // System.out.println(arr);    
-        }
-        // System.out.println("Time"+ time);
-        double median =0;
-        if(arr.size()%2==0){
-            median = (arr.get(arr.size()/2 - 1) + arr.get(arr.size()/2 ) )/2.0;
-        }else{
-            median = arr.get(arr.size()/2 );
         }
 
-        return median;
-        
+
+
+
     }
+        
 }
