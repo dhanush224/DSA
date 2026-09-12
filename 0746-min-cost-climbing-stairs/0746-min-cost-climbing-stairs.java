6491de129eccb98c6c8dfa05581b6b443330fa34
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-12 14:36:49 -0700

    Time: 0 ms (100%), Space: 44.9 MB (58.76%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,22 +1,18 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
 
-        int[] dp = new int[cost.length];
-        for(int i=0;i<cost.length;i++) dp[i]=-1;
-
-        return Math.min(recursion(0,cost,dp),recursion(1,cost,dp));        
-    }
-
-    public int recursion(int i, int[] cost, int[] dp){
-
-        if(i>=cost.length) return 0;
-
-        if(dp[i]==-1){
-            int step1 = cost[i] + recursion(i+1,cost,dp);
-            int step2 = cost[i] + recursion(i+2,cost,dp);
-            dp[i]= Math.min(step1,step2);
+        int prev=cost[0];
+        int prev2=prev;
+        for(int i=1;i<cost.length;i++){
+            int ss = prev;
+            if(i==1) ss =0;
+            int ds = Integer.MAX_VALUE;
+            if(i-2>=0) ds = prev2;
+            prev2=prev;
+            prev=cost[i]+Math.min(ss,ds); 
         }
-
-        return dp[i];
+        return Math.min(prev,prev2);        
     }
+
+
 }
