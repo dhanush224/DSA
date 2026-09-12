6a914e28f58d5aaa2f92b68f593b9cf4d33e236d
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-11 21:38:03 -0700

    Time: 0 ms (100%), Space: 42.9 MB (41.3%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,38 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
+        // if(nums.length==2) return Math.max(nums[0], nums[i+1]);
+
         int[] dp = new int[nums.length];
         for(int i=0;i<nums.length;i++) dp[i]=-1;
-        return recursion(0,nums,dp,0);        
+
+        int[] dp2 = new int[nums.length];
+        for(int i=0;i<nums.length;i++) dp2[i]=-1;
+        
+        return Math.max(recursion(0,nums.length-1,nums,dp), recursion(1,nums.length,nums,dp2));        
     }
 
-    public int recursion(int i, int[] nums, int[] dp, int flag){
-        if(nums.length==1) return nums[i];
-        if(nums.length==2) return Math.max(nums[i], nums[i+1]);
-        if(i>=nums.length) return 0;
+    public int recursion(int i, int end, int[] nums, int[] dp){
+
+        if(i>=end) return 0;
 
         if(dp[i]==-1){
-        int pick=-1;
-        if(i==0){
-            flag=1;
-            pick = recursion(i+2,nums,dp,flag);
-            flag=0;
-        }else{
-            pick = nums[i] + recursion(i+2,nums,dp,flag);
-        }
-
-        if(i==nums.length-1 && flag==1){
-             pick = Math.max(nums[i],nums[0]) + recursion(i+2,nums,dp,flag);
-        }else if(i==nums.length-2 && flag==1) pick = nums[0]+nums[i] + recursion(i+2,nums,dp,flag);
-
-        if(i==0){
-            dp = new int[nums.length];
-            for(int j=0;j<nums.length;j++) dp[j]=-1;
-        }
-        int notpick = recursion(i+1,nums,dp,flag);
+        int pick= nums[i] + recursion(i+2,end,nums,dp);
+        int notpick = recursion(i+1,end,nums,dp);
         dp[i] = Math.max(pick,notpick);            
-        
         }
-        
         return dp[i];
     }
 }
