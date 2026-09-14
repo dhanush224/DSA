95ddeea7d68a2383c3e5ddb105f9e4b3a5d476a6
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-13 19:52:19 -0700

    Time: 0 ms (100%), Space: 43 MB (8.46%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
         if(nums.length==1) return nums[0];
-        // if(nums.length==2) return Math.max(nums[0], nums[i+1]);
 
-        int[] dp = new int[nums.length];
-        for(int i=0;i<nums.length;i++) dp[i]=-1;
+        int prev=nums[0];
+        int prev2=prev;
 
-        int[] dp2 = new int[nums.length];
-        for(int i=0;i<nums.length;i++) dp2[i]=-1;
-        
-        return Math.max(recursion(0,nums.length-1,nums,dp), recursion(1,nums.length,nums,dp2));        
-    }
-
-    public int recursion(int i, int end, int[] nums, int[] dp){
-
-        if(i>=end) return 0;
-
-        if(dp[i]==-1){
-        int pick= nums[i] + recursion(i+2,end,nums,dp);
-        int notpick = recursion(i+1,end,nums,dp);
-        dp[i] = Math.max(pick,notpick);            
+        for(int i=1;i<nums.length-1;i++){
+            int l = nums[i];
+            if(i>1) l+=prev2;
+            int r = prev;
+            prev2=prev;
+            prev=Math.max(l,r);
         }
-        return dp[i];
+        int max = prev;
+        prev=nums[1];
+        prev2=prev;
+        for(int i=2;i<nums.length;i++){
+            int l = nums[i];
+            if(i>2) l+=prev2;
+            int r = prev;
+            prev2=prev;
+            prev=Math.max(l,r);
+        }
+        
+        return Math.max(prev,max);        
     }
 }
