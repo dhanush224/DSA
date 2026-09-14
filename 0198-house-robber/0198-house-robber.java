99d628443b1a457baea5823839c77d4f5f871486
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-13 19:43:05 -0700

    Time: 0 ms (100%), Space: 42.8 MB (56.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,21 +3,15 @@ class Solution {
         if(nums.length==1) return nums[0];
         int n = nums.length;
         int[] dp = new int[n];
-        for(int i=2;i<n;i++)dp[i]=-1;
         
         dp[0]=nums[0];
-        dp[1]=nums[1];
-        for(int i=2;i<nums.length;i++){
-            int max= Integer.MIN_VALUE;
-            for(int j=i;j>=0;j--){
-                if(j>1){
-                    int val = (dp[i-j]!=-1)? dp[i-j] : nums[i-j];
-                    if(val>max) max=val;
-                }
-            }
-            dp[i]=max+nums[i];
+        for(int i=1;i<nums.length;i++){
+            int l = nums[i];
+            if(i>1) l+=dp[i-2];
+            int r = dp[i-1];
+            dp[i]=Math.max(l,r);
         }
 
-        return Math.max(dp[n-1],dp[n-2]);
+        return dp[n-1];
     }
 }
