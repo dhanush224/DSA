797c06a0b148803f75311bf0f69c29f783068744
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-13 19:30:13 -0700

    Time: 1 ms (1.99%), Space: 42.7 MB (56.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        for(int i=0;i<nums.length;i++) dp[i]=-1;
-        return recursion(0,nums,dp);
-    }
-
-    public int recursion(int i, int[] nums, int[] dp){
-        if(i>=nums.length)return 0;
-
-        if(dp[i]==-1){
-            int pick = nums[i] + recursion(i+2,nums,dp);
-            int notpick = recursion(i+1,nums,dp);
-            dp[i] = Math.max(pick,notpick);
+        if(nums.length==1) return nums[0];
+        int n = nums.length;
+        int[] dp = new int[n];
+        for(int i=2;i<n;i++)dp[i]=-1;
+        
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+        for(int i=2;i<nums.length;i++){
+            int max= Integer.MIN_VALUE;
+            for(int j=i;j>=0;j--){
+                if(j>1){
+                    int val = (dp[i-j]!=-1)? dp[i-j] : nums[i-j];
+                    if(val>max) max=val;
+                }
+            }
+            dp[i]=Math.max(max+nums[i],nums[i]);
         }
-        return dp[i];
+
+        return Math.max(dp[n-1],dp[n-2]);
     }
 }
