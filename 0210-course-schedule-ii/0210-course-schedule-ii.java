78d085c37d6765fba7243a0c25c73f537321676e
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-16 13:53:05 -0700

    Time: 5 ms (80.42%), Space: 46.9 MB (39.43%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -2,43 +2,38 @@ class Solution {
     int index=0;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
 
-        int[] vis = new int[numCourses];
-        int[] path = new int[numCourses];
-        int[] order = new int[numCourses];
+        int[] indegree = new int[numCourses];
 
         List<List<Integer>> list = new ArrayList<>();
         for(int i=0;i<numCourses;i++) list.add(new ArrayList<>());
-
-        for(int[] pair: prerequisites){
+        for(int[] pair: prerequisites) {
+            indegree[pair[1]]++;
             list.get(pair[0]).add(pair[1]);
         }
 
+
+        Queue<Integer> q = new ArrayDeque<>();
         for(int i=0;i<numCourses;i++){
-            if(vis[i]!=1){
-                if(!recursion(i,list,vis,path,order)) return new int[]{};
-            }
+            if(indegree[i]==0) q.offer(i);
         }
 
-        return order;
-        
-    }
-
-    public boolean recursion(int node, List<List<Integer>> list, int[] vis, int[] path, int[] order){
-
-        vis[node]=1;
-        path[node]=1;
-
-        for(int nei: list.get(node)){
-            if(path[nei]==1) return false;
-            if(vis[nei]!=1){
-                if(!recursion(nei,list,vis,path,order)) return false;
+        int[] order = new int[numCourses];
+        List<Integer> ret = new ArrayList<>();
+        while(!q.isEmpty()){
+            int course = q.poll();
+            ret.add(course);
+            for(int nei: list.get(course)){
+                if(--indegree[nei]==0)q.offer(nei);
             }
+
         }
 
-        path[node]=0;
-        order[index++]=node;
 
-
-        return true;
+        if(ret.size()<numCourses) return new int[]{};
+        else {
+            for(int i=0;i<numCourses;i++) order[i]=ret.get(numCourses-i-1);
+            return order;
+        }
     }
+
 }
