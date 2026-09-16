ec3e3d733faf5de4d26c691c6cdce4296d5e9593
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-15 19:10:46 -0700

    Time: 3 ms (29.52%), Space: 47.6 MB (74.89%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,55 +1,57 @@
 class Solution {
     public void solve(char[][] board) {
-        
-        int[][] vis = new int[board.length][board[0].length];
+        int m = board.length;
+        int n = board[0].length;
 
-        //start the traversals
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='O' && vis[i][j]!=1){
-                    if(i!=0 && j!=0 && i!=board.length-1 && j!=board[0].length-1){  
-                        List<int[]> regionList = new ArrayList<>();                    
-                        if(dfs(i,j, board, regionList,vis,-1)!=0){
-                            //convert Os to Xs
-                            regionList.add(new int[]{i,j});
-                            convert(board,regionList);
-                        }
-                    }
-
-                } 
+        int[][] vis = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        for(int j=0;j<n;j++) {
+        if(board[0][j]=='O' && vis[0][j]!=1) {
+            q.offer(new int[]{0,j});
+            vis[0][j]=1;
+        }}
+        for(int j=0;j<n;j++) {
+            if(board[m-1][j]=='O' && vis[m-1][j]!=1) {
+            q.offer(new int[]{m-1,j});
+            vis[m-1][j]=1;
+        }}
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O' && vis[i][0]!=1){
+                q.offer(new int[]{i,0});
+                vis[i][0]=1;
             }
         }
-        
-    }
+        for(int i=0;i<m;i++){
+            if(board[i][n-1]=='O' && vis[i][n-1]!=1){
+                q.offer(new int[]{i,n-1});
+                vis[i][0]=1;
+            }
+        }
 
-    public int dfs(int i, int j, char[][] board, List<int[]> regionList, int[][] vis, int flag){
-        int[][] dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        vis[i][j]=1;
-        
-        for(int[] d: dirs){
-            int nr = i+d[0];
-            int nc = j+d[1];
+        int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int[] curr = q.poll();
 
-            if(nr>=0 && nc>=0 && nr<board.length && nc<board[0].length && board[nr][nc]=='O' && vis[nr][nc]!=1){
+                for(int[] d: dirs){
+                    int nr = curr[0]+d[0];
+                    int nc = curr[1]+d[1];
 
-                if(nr==0 || nc==0 || nr==board.length-1 || nc==board[0].length-1){
-                    //not a region
-                    flag=0;
-                    flag = dfs(nr,nc,board,regionList,vis,flag);
-                }else{
-                    regionList.add(new int[]{nr,nc});
-                    flag = dfs(nr,nc,board,regionList,vis,flag);
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && board[nr][nc]=='O' && vis[nr][nc]!=1){
+                        vis[nr][nc]=1;
+                        q.add(new int[]{nr,nc});
+                    }
                 }
             }
         }
 
-        return flag;
+        for(int i=1;i<m-1;i++){
+            for(int j=1;j<n-1;j++){
+                if(vis[i][j]!=1) board[i][j]='X';
+            }
+        }
+
     }
 
-    public void convert(char[][] board, List<int[]> regionList){
-        for(int[] cell: regionList){
-            board[cell[0]][cell[1]] = 'X';
-        }
-        regionList = new ArrayList<>();
-    }
 }
