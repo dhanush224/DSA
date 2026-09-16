1df45249e2bf22d3371d9c004d38cc71d101143b
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-15 18:51:56 -0700

    Time: 7 ms (95.29%), Space: 50.4 MB (53.69%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.java
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -2,12 +2,12 @@ class Solution {
     public void wallsAndGates(int[][] rooms) {
         int m = rooms.length;
         int n = rooms[0].length;
+
         Queue<int[]> q = new ArrayDeque<>();
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(rooms[i][j]==0){
-                    q.add(new int[]{i,j,0});
-                }
+                if(rooms[i][j]==0) q.offer(new int[]{i,j});
             }
         }
 
@@ -15,16 +15,19 @@ class Solution {
         while(!q.isEmpty()){
             int size = q.size();
             for(int i=0;i<size;i++){
-                int[] curr = q.remove();
+                int[] curr = q.poll();
+
                 for(int[] d: dirs){
                     int nr = curr[0]+d[0];
                     int nc = curr[1]+d[1];
-                    if(nr>=0 && nc>=0 && nr<m && nc<n && rooms[nr][nc]==Integer.MAX_VALUE){
-                        rooms[nr][nc]=curr[2]+1;
-                        q.add(new int[]{nr,nc,curr[2]+1});
+
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && rooms[nr][nc]==Integer.MAX_VALUE){
+                        rooms[nr][nc]=1+rooms[curr[0]][curr[1]];
+                        q.add(new int[]{nr,nc});
                     }
                 }
             }
+
         }
     }
 
