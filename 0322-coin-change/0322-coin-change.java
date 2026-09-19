eeb7cef7be57d850c48535d162389557165aedbe
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-18 18:35:03 -0700

    Time: 19 ms (39.93%), Space: 47.6 MB (10.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,22 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         long[][] dp = new long[coins.length][amount+1];
-        long ans = recursion(coins.length-1,coins,amount,dp);
-        if(ans>=Integer.MAX_VALUE) return -1;
-        else return (int)ans;
-    }
-
-    public long recursion(int i, int[] coins, int amount,long[][] dp){
-        if(i==0){
-            if(amount%coins[i]==0) return amount/coins[i];
-            else return Integer.MAX_VALUE;
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0) dp[0][t]=t/coins[0];
+            else dp[0][t]=Integer.MAX_VALUE;
         }
 
-        if(dp[i][amount]!=0) return dp[i][amount];
-        long notpick = recursion(i-1,coins,amount,dp);
-        long pick = Integer.MAX_VALUE;
-        if(coins[i]<=amount) pick = 1+ recursion(i,coins,amount-coins[i],dp);
-        dp[i][amount]=Math.min(pick,notpick);
-        return dp[i][amount];
+        for(int i=1;i<coins.length;i++){
+            for(int t=0;t<=amount;t++){
+                long notpick = dp[i-1][t];
+                long pick = Integer.MAX_VALUE;
+                if(coins[i]<=t) pick = 1+ dp[i][t-coins[i]];
+
+                dp[i][t]=Math.min(notpick,pick);
+            }
+        }
+
+        if(dp[coins.length-1][amount]>=Integer.MAX_VALUE) return -1;
+        else return (int)dp[coins.length-1][amount];
+
     }
 }
