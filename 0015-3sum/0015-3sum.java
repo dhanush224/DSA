abac222cb0b1aec80fce34846068ebcaebe53326
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-20 10:53:17 -0700

    Time: 36 ms (28.12%), Space: 59.2 MB (45.37%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,51 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        Arrays.sort(nums);      
-        
-        List<List<Integer>> ans = new ArrayList<>();
-
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        int j=0;
+        int k=nums.length-1;
         for(int i=0;i<nums.length-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-                // System.out.println("PrevI: "+prevI);
-                int j=i+1,k=nums.length-1;
-                while(j<k){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    if(sum==0){
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        // System.out.println("nums[i]:"+nums[i]+", i:"+i);
-                        // System.out.println("nums[j]:"+nums[j]+", j:"+j);
-                        // System.out.println("nums[k]:"+nums[k]+", k:"+k);
-                        // System.out.println("PrevJ: "+prevJ);
-                        // System.out.println("PrevK: "+prevK);        
-
-                        j++;
-                        k--;
-                        ans.add(list);
-
-                        while(j<k && nums[j]==nums[j-1]) j++;
-                        // while(j<k && nums[k]==nums[k+1]) k--;
-                    }else if(sum<0){
-                        j++;
-                    }
-                    else{
-                        k--;
-                    }
-
-                }
-            
+            j=i+1;
+            k=nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    List<Integer> innerList = new ArrayList<>(List.of(nums[i],nums[j],nums[k]));
+                    list.add(innerList);
+                    while(j+1<nums.length && nums[j]==nums[j+1]) j++;
+                    j++;
+                    k--;
+                }else if(sum>0) k--;
+                else j++;
+            }
+            while(i+1<nums.length && nums[i]==nums[i+1])i++;
         }
 
+        return list;
 
-
-
-        return ans;
-        
-
-
-        
     }
 }
