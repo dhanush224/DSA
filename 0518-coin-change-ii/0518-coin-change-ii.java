ec5371b29302322c5d07c14d2bbe72fba4936194
dhanush224 <68319689+dhanush224@users.noreply.github.com>
2026-09-20 12:11:17 -0700

    Time: 24 ms (22.23%), Space: 57.2 MB (29.35%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -8,23 +8,22 @@ class Solution {
             }
         }
 
-        int ret = recursion(coins.length-1,coins,amount,dp);
-
-        return ret;
-    }
-
-    public int recursion(int i,int[] coins, int amount, int[][] dp){
-        if(i==0){
-            if(amount%coins[i]==0) return 1;
-            else return 0;
+        for(int j=0;j<=amount;j++){
+            if(j%coins[0]==0)dp[0][j]=1;
+            else dp[0][j]=0;
         }
 
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        int notpick = recursion(i-1,coins,amount,dp);
-        int pick = 0;
-        if(amount>=coins[i]) pick = recursion(i,coins,amount-coins[i],dp);
-
-        dp[i][amount]=pick+notpick;
-        return dp[i][amount];
+        for(int i=1;i<coins.length;i++){
+            for(int j=0;j<=amount;j++){
+                int notpick = dp[i-1][j];
+                int pick = 0;
+                if(j>=coins[i])pick = dp[i][j-coins[i]];
+                if(pick<0) pick =0;
+                if(notpick<0) notpick=0;
+                dp[i][j]=pick+notpick;
+            }
+        }
+        return dp[coins.length-1][amount];
     }
+
 }
